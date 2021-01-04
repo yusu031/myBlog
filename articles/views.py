239e49ce94d173612de052c2862e2006110838dc
名称 <>
2021-01-04 20:58:54 +0800

    创建新的django前端

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Articles
 
-# Create your views here.
+# FBV     function based view  基于函数的视图
+
+
+def index(request):
+    articles = Articles.objects.all()
+    context = {
+        "articles":articles
+    }
+    return render(request,'index.html',context)
+
+
+def detail(request,pk):
+    article = Articles.objects.get(pk=pk)
+    context = {
+        'article':article
+    }
+    return render(request,'single_article.html',context)
+
+
+def contact(request):
+    return render(request,'contact.html')
+
+
+def about(request):
+    return render(request,'about.html')
+
+
